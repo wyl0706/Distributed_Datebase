3da08b8e5381c39ff7fdde339562edaff05ac8e0
WYL <wyl0706@gmail.com>
2015-06-16 22:27:51 +0800

    a commit in 2015/06/16

---
+++ src/edu/guet/distributedatebase/TM/TransactionManger.java
@@ -0,0 +1,10 @@
+package edu.guet.distributedatebase.TM;
+
+/**
+ * Created by Caesar on 2015/6/16.
+ */
+public class TransactionManger {
+    public void TM() {
+
+    }
+}
--- src/edu/guet/distributedatebase/WC/WorkflowControl.java
+++ src/edu/guet/distributedatebase/WC/WorkflowControl.java
@@ -1,7 +1,42 @@
 package edu.guet.distributedatebase.WC;
 
+import java.util.List;
+
 /**
  * Created by Caesar on 2015/6/16.
  */
 public class WorkflowControl {
+    //预约行程
+    public void reserveItinerary
+    (int xid, String custName, List flightNumList, String location, boolean needCar, boolean needRoom) {
+
+    }
+
+    //修改行程
+    public void changeItinerary
+    (int xid, String custName, List flightNumList, String location, boolean needCar, boolean needRoom) {
+
+    }
+
+    //取消行程
+    public void cancelItinerary
+    (int xid, String custName, List flightNumList, String location, boolean needCar, boolean needRoom) {
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
